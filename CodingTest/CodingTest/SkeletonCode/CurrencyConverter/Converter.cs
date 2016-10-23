896367986d7ee06deb97a9137c71beebc05bfd2b
Andrew MAughan <andrew@maughan.org.uk>
2016-10-23 21:38:17 +0100

    Change CurrencyConverter to convert USD to GBP and GBP to USD

--- CodingTest/CodingTest/SkeletonCode/CurrencyConverter/Converter.cs
+++ CodingTest/CodingTest/SkeletonCode/CurrencyConverter/Converter.cs
@@ -1,9 +1,20 @@
-﻿namespace SkeletonCode.CurrencyConverter
+﻿using System;
+
+namespace SkeletonCode.CurrencyConverter
 {
 	public class Converter
 	{
 		public decimal Convert(string inputCurrency, string outputCurrency, decimal amount)
 		{
+            decimal conversionRatio = 1.25m;
+
+            if(inputCurrency == "USD" && outputCurrency == "GBP")
+            {
+                conversionRatio = 0.8m;
+            }
+
+            amount = Decimal.Multiply(amount, conversionRatio);
+
 			return amount;
 		}
 	}
