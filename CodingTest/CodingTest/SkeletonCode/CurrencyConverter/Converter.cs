357ceea42814a69528c68382285e31ae0369c69b
Andrew MAughan <andrew@maughan.org.uk>
2016-10-23 21:43:51 +0100

    Complete CurrencyConverter: throw exception if unknown currency type passed

--- CodingTest/CodingTest/SkeletonCode/CurrencyConverter/Converter.cs
+++ CodingTest/CodingTest/SkeletonCode/CurrencyConverter/Converter.cs
@@ -6,6 +6,11 @@ namespace SkeletonCode.CurrencyConverter
 	{
 		public decimal Convert(string inputCurrency, string outputCurrency, decimal amount)
 		{
+            if ((inputCurrency != "USD" && inputCurrency != "GBP") || (outputCurrency != "USD" && outputCurrency != "GBP"))
+            {
+                throw new Exception("Error");
+            }
+
             decimal conversionRatio = 1.25m;
 
             if(inputCurrency == "USD" && outputCurrency == "GBP")
