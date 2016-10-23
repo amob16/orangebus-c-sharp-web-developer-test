eba800e8460000874f7659a27e011f9ef469d576
Andrew MAughan <andrew@maughan.org.uk>
2016-10-23 20:26:30 +0100

    Completed Jamsmin Test: camelCaseToSpineCase

--- CodingTest/CodingTest/JasmineTests/scripts/string-extensions.js
+++ CodingTest/CodingTest/JasmineTests/scripts/string-extensions.js
@@ -1,6 +1,14 @@
 ﻿String.prototype.capitalise = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
-String.prototype.camelCaseToSpineCase = function () { }
+String.prototype.camelCaseToSpineCase = function () {
+    // [\s] - handle whitespace
+    // or (|)
+    // lowercase and numbers (a-z0-9)
+    // look ahead and check for uppercase
+    // replace with -
+    // cast everything to lowercase
+    return this.replace(/[\s]+|([a-z0-9])(?=[A-Z])/g, "$1-").toLowerCase();
+}
 String.prototype.spineCaseToCamelCase = function () { }
 String.prototype.format = function () { }
