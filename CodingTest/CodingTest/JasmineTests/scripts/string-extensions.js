42617440761ad1b702defeb43f69afdf18e82d9d
Andrew MAughan <andrew@maughan.org.uk>
2016-10-23 20:14:53 +0100

    Completed Jamsmin Test Capitilise

--- CodingTest/CodingTest/JasmineTests/scripts/string-extensions.js
+++ CodingTest/CodingTest/JasmineTests/scripts/string-extensions.js
@@ -1,4 +1,6 @@
-﻿String.prototype.capitalise = function () {}
+﻿String.prototype.capitalise = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 String.prototype.camelCaseToSpineCase = function () { }
 String.prototype.spineCaseToCamelCase = function () { }
 String.prototype.format = function () { }
