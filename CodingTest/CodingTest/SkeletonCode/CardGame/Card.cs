09361d9df59e3b206ba11f3e8f7e0ac1fdc95b57
Andrew MAughan <andrew@maughan.org.uk>
2016-10-23 22:00:59 +0100

    Implement methods for Card

--- CodingTest/CodingTest/SkeletonCode/CardGame/Card.cs
+++ CodingTest/CodingTest/SkeletonCode/CardGame/Card.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace SkeletonCode.CardGame
 {
-    class Card
+    class Card : ICard
     {
+        private string suit;
+        private string value;
+
+        // Constructor - set the suit name and value
+        // @param string s (suit)
+        // @param string v (value)
+        public Card(string s, string v)
+        {
+            suit = s;
+            value = v;
+        }
+
+        // return the suit name
+        public string getSuit()
+        {
+            return suit;
+        }
+
+        // return the value of the card
+        public string getValue()
+        {
+            return value;
+        }
+
+        // combine the suit name and value
+        public string getCardName()
+        {
+            return value+" of "+suit;
+        }
     }
 }
